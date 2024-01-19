266941e1e48fad201f9388e1f0aa3fcd1a7512cc
floydferrer <floydferrer@hotmail.com>
2024-01-18 23:23:49 -0800

    completed all steps

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, flash, jsonify, make_response, session
+from flask import Flask, request, render_template, redirect, jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -10,17 +10,27 @@ debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
-@app.route('/reset')
+@app.route('/start-game')
+def create_board():
+    session['board'] = []
+    session['highscore'] = 0
+    session['nplays'] = 0
+    board = boggle_game.make_board()
+    print(board)
+    session['board'] = board
+    return redirect('/boggle')
+
+@app.route('/restart')
 def reset_board():
     session['board'] = []
     board = boggle_game.make_board()
+    print(board)
     session['board'] = board
     return redirect('/boggle')
 
 @app.route('/boggle')
 def display_board():
-    board = session['board']
-    return render_template('boggle.html', board=board)
+    return render_template('boggle.html', board=session['board'], highscore=session['highscore'], nplays=session['nplays'])
 
 @app.route('/words', methods=['POST'])
 def display_words():
@@ -28,9 +38,18 @@ def display_words():
     word_list = boggle_game.words
     return render_template('words.html', word_list=word_list)
 
-@app.route('/floyd', methods=['POST'])
+@app.route('/check', methods=['GET'])
 def show_word():
     board = session['board']
     word = request.args['word']
-    answer = Boggle.check_valid_word(board, word)
+    answer = boggle_game.check_valid_word(board, word)
     return jsonify({'result': answer})
+
+@app.route('/stats', methods=['POST'])
+def update_stats():
+    score = request.json['score']
+    highscore = session.get('highscore', 0)
+    nplays = session.get('nplays', 0)
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+    return jsonify(brokeRecord=score > highscore)
--- static/app.js
+++ static/app.js
@@ -1,29 +1,60 @@
+const $timer = $('#timer');
+const $score = $('#score');
 const $guessForm = $('#guess-form');
+const $guessButton = $('#guess-btn');
+const $submit = $('#submit');
+const $result = $('#result');
+const $words = $('#words');
+const correctWords = [];
+let score = 0;
+let attempts = 0;
 
-$guessForm.on('submit', async handleSubmit(evt){
+$guessForm.on('submit', async function handleSubmit(evt){
     evt.preventDefault();
-    console.log($guessForm[0][0].value);
     let word = $guessForm[0][0].value;
-    // $guessForm[0].reset();
-    const response = await axios("/floyd", { params: { word: word } });
-    // console.log(response.data.result);
-    // const response = await axios({
-    //     url: '/floyd',
-    //     method: "GET",
-    // });
-    console.log(response.data.result);
+    $guessForm[0].reset();
+    const response = await axios("/check", { params: { word: word } });
+    if (response.data.result === 'ok' && correctWords.indexOf(word) === -1) {
+        score += word.length;
+        $score.text(`Score: ${score}`);
+        correctWords.push(word);
+        $words.append(`<li>${word}</li>`);
+        $result.text(response.data.result);
+        console.log(score);
+    } else if (response.data.result === 'ok' && correctWords.indexOf(word) !== -1) {
+        $result.text('Already found!');
+    } else if (response.data.result !== 'ok') {
+        $result.text(response.data.result);
+    }
 });
 
-// async function handleSubmit(evt) {
-//     evt.preventDefault();
-//     console.log($guessForm[0][0].value);
-//     let word = $guessForm[0][0].value;
-//     // $guessForm[0].reset();
-//     const response = await axios("/floyd", { params: { word: word } });
-//     // console.log(response.data.result);
-//     // const response = await axios({
-//     //     url: '/floyd',
-//     //     method: "GET",
-//     // });
-//     console.log(response.data.result);
-// }
+async function scoreGame(){
+    const response = await axios.post("/stats", { score: score });
+    if (response.data.brokeRecord){
+        $score.text(`New Record: ${score}`);
+    } else {
+        $score.text(`Final score: ${score}`);
+    }
+}
+
+function handleTimer(){
+    let counter = 60;
+    let intervalId = setInterval(() => {
+        if (counter >= 1) {
+            counter --;
+            $timer.text(`Timer: ${counter}`);
+        } else {
+            clearInterval(intervalId);
+            disableForm(true);
+            scoreGame();
+        }
+    }, 1000)
+}
+
+function disableForm(value) {
+    $guessForm[0][0].value = "";
+    $guessButton.attr('disabled', value);
+    $submit.attr('disabled', value);
+}
+
+handleTimer();
